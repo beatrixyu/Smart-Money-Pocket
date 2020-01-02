1d761d96858e3bdac89397ab0d7145901a6dbd30
beatrixyu <beatrixyu@126.com>
2020-01-03 00:08:26 +0100

    Updates

---
+++ precache-manifest.e77a94023a3e11175c4a443e728651fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc4cc8535170d077485731e9a552b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c92d42c7637b927812",
+    "url": "/static/css/main.3e557bd9.chunk.css"
+  },
+  {
+    "revision": "4c465f6650f8878d9e4d",
+    "url": "/static/js/2.a81ced3c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a81ced3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2c92d42c7637b927812",
+    "url": "/static/js/main.1e1a81b2.chunk.js"
+  },
+  {
+    "revision": "ee5154daf6f3df947295",
+    "url": "/static/js/runtime-main.b4af6cea.js"
+  }
+]);
